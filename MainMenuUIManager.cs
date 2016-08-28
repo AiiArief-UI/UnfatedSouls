945a212515d7e57e20a356d9d784e5fe334c8142
Muhammad Assagaf <muhammadassagaf46@gmail.com>
2016-08-28 15:11:30 +0700

    push script lama

---
+++ MainMenuUIManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUIManager : MonoBehaviour {
+
+	public GameObject mainMenu, settingPanel, memoryPanel;
+
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OpenSetting(){
+		mainMenu.SetActive (false);
+		settingPanel.SetActive (true);
+	}
+
+	public void OpenMemory(){
+		mainMenu.SetActive (false);
+		memoryPanel.SetActive (true);
+	}
+
+
+	public void Back(){
+		settingPanel.SetActive (false);
+		memoryPanel.SetActive (false);
+		mainMenu.SetActive (true);
+	}
+
+	public void LoadLevel(string level){
+		Application.LoadLevel(level);
+	}
+
+	public void Quit(){
+		Debug.Log ("Game QUIT");
+		Application.Quit();
+	}
+}
